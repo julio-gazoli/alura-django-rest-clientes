212f75797664741fddda1de261884e1d2f88fb7a
julio.gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-07-13 08:01:07 -0300

    Validando todos os campos / Melhorando o código

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 
 class ClienteSerializer(serializers.ModelSerializer):
@@ -7,7 +8,14 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError('O CPF deve ter 11 dígitos')
-        return cpf
+    def validate(self, data):
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError(
+                {'cpf': 'O CPF deve ter 11 dígitos'})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError(
+                {'nome': 'Não inclua números neste campo'})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError(
+                {'rg': 'O RG deve ter 9 dígitos'})
+        return data
